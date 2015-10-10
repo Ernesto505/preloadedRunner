bda839dcfdcd3490f7b47b10efbaad24e22abe6e
Petros <ernesto_mwc@hotmail.com>
2015-10-11 00:14:06 +0100

    Initial Commit - Some random level generation and basic character movement

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterController : MonoBehaviour {
+
+    // Use this for initialization
+    public float thrust;
+    Rigidbody rb;
+    void Start()
+    {
+        rb = this.gameObject.GetComponent<Rigidbody>();
+        Debug.Log("Actual position" + this.gameObject.transform.position);
+    }
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(-transform.right * thrust);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(transform.right * thrust);
+        }
+        float zAngle = (this.gameObject.transform.eulerAngles.z+270.0f)*Mathf.PI/180.0f;
+        Debug.Log(this.gameObject.transform.eulerAngles.z);
+        float x = 4.0f * Mathf.Cos(zAngle);
+        float y = 4.0f * Mathf.Sin(zAngle);
+        Debug.Log("Calculated position x" + x);
+        Debug.Log("Calculated position y" + y);
+        //     Debug.Log("Actual position" + this.gameObject.transform.position.x);
+        Debug.Log("Actual position" + this.gameObject.transform.position);
+    }
+}
---
+++ Assets/Scripts/CoinManager.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CoinManager : MonoBehaviour {
+
+    public Rigidbody Coins;
+
+	// Use this for initialization
+	void Start () {
+	    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float randomAngle = Random.Range(180, 360);
+        float zAngle = randomAngle * Mathf.PI / 180.0f;
+        float x = 4.0f * Mathf.Cos(zAngle);
+        float y = 4.0f * Mathf.Sin(zAngle);
+        Vector3 coinPosition = new Vector3(x, y, 20.0f);
+        Rigidbody newCoin = (Rigidbody) Instantiate(Coins, coinPosition, Quaternion.identity);
+        newCoin.velocity = new Vector3(0, 0, -10.0f);
+	}
+}
