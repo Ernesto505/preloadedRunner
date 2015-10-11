5ace296055d3db58db8f89ebde47e8c2f7049a87
Petros <ernesto_mwc@hotmail.com>
2015-10-11 01:31:44 +0100

    Simple counter for hits added

---
+++ Assets/Scripts/Coin.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Coin : MonoBehaviour {
+
+    public delegate void CoinHitHandler();
+
+    public CoinHitHandler Coinhit;
+
+    // Use this for initialization
+    void Start () {
+	    this.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, -10.0f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (transform.position.z < -15.0f)
+        {
+            Destroy(gameObject);
+        }
+	}
+
+    void OnTriggerEnter()
+    {
+
+        Destroy(gameObject);
+
+        if(Coinhit != null)
+        {
+            Coinhit();
+        }
+    }
+    
+}
--- Assets/Scripts/CoinManager.cs
+++ Assets/Scripts/CoinManager.cs
@@ -1,23 +1,48 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class CoinManager : MonoBehaviour {
 
     public Rigidbody Coins;
+    int m_coinsHit = 0;
+    int m_updateCounter = 0;
+
+    public Text CoinsCounterText;
 
 	// Use this for initialization
-	void Start () {
-	    
+	void Start ()
+    {
+
 	}
-	
-	// Update is called once per frame
-	void Update () {
-        float randomAngle = Random.Range(180, 360);
-        float zAngle = randomAngle * Mathf.PI / 180.0f;
-        float x = 4.0f * Mathf.Cos(zAngle);
-        float y = 4.0f * Mathf.Sin(zAngle);
-        Vector3 coinPosition = new Vector3(x, y, 20.0f);
-        Rigidbody newCoin = (Rigidbody) Instantiate(Coins, coinPosition, Quaternion.identity);
-        newCoin.velocity = new Vector3(0, 0, -10.0f);
-	}
+
+    public void Subscribe(Coin myCoin)
+    {
+        myCoin.Coinhit += new Coin.CoinHitHandler(CoinHasBeenHit);
+    }
+
+    // Update is called once per frame
+    void Update ()
+    {
+        m_updateCounter++;
+
+        if (m_updateCounter % 30==0)
+        { 
+            float randomAngle = Random.Range(180, 360);
+            float zAngle = randomAngle * Mathf.PI / 180.0f;
+            float x = 4.0f * Mathf.Cos(zAngle);
+            float y = 4.0f * Mathf.Sin(zAngle);
+            Vector3 coinPosition = new Vector3(x, y, 20.0f);
+            Rigidbody newCoin = (Rigidbody) Instantiate(Coins, coinPosition, Quaternion.identity);
+            Subscribe(newCoin.gameObject.GetComponent<Coin>());
+            m_updateCounter = 0;
+        }
+    }
+
+    void CoinHasBeenHit()
+    {
+        m_coinsHit++;
+
+        CoinsCounterText.text = m_coinsHit.ToString();
+    }
 }
